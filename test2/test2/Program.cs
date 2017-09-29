ff2104aaf5a40eb2c0c681392f92d8ce8758077d
jparkCreations <jjp0009@uah.edu>
2017-09-29 14:15:53 -0500

    Add files via upload
    Files used to upload into agent and agency

---
+++ test2/test2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "Data Source=DESKTOP-KFI49LK;Initial Catalog=Housing;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string name = "Destruction";
+                string email = "death.org";
+                string phone = "6669996666";
+                string street = "666 Street";
+                string state = "666 City";
+                string city = "999 Death Row";
+                string agency_zip = "66666";
+
+                string fName = "Jake";
+                string lName = "stateFarm";
+                string userName = "Jake";
+                string password = "fromStateFarm";
+                string number = "345559999";
+                string email2 = "Jake@stateFarm";
+                int agency_id = 5;
+                string number2 = "1234567890";
+
+
+                //Int16 zip = 3580;
+
+                //int zip = 45678;
+               // string q = "insert into agency(agency_name,agency_email,agency_phone,agency_street,agency_state,agency_city,agency_zip)values('" + name + "','" + email + "','" + phone + " ','" + street + "','" + state + " ','" + city + "','" + agency_zip + "')";
+                //SqlCommand cmd = new SqlCommand(q, con);
+                //cmd.ExecuteNonQuery();
+
+                string z = "insert into agent(agent_Fname,agent_Lname,agent_Uname,agent_password,agent_number,agent_email,agency_id,agent_number2)values('" + fName + "','" + lName + "','" + userName + " ','" + password + "','" + number + " ','" + email2 + "','" + agency_id + "','" + number2 + "')";
+
+                SqlCommand cmd1 = new SqlCommand(z, con);
+                cmd1.ExecuteNonQuery();
+
+                //MessageBox.Show("Connection made successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect");
+            }
+        }
+    }
+}
